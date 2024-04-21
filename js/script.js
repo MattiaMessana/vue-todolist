d80085acd20ea25106d3ac4a03a9ad02a183d342
MattiaMessana <mattia.messana@icloud.com>
2024-04-21 18:36:20 +0200

    add:local storage

--- js/script.js
+++ js/script.js
@@ -8,26 +8,33 @@ const app = createApp({
                 done: false,
             },
 
-            toDo: [
-                {
-                    text: "Querterly Newsletters",
-                    done: false,
-                },
-                {
-                    text: "spesa",
-                    done: false,
-                },
-            ],
+            toDo: [],
         };
     },
     created() {
-        
+        //all avvio della pagina controllo se nel local storage c'è la chiave toDo
+        const localToDo = localStorage.getItem("toDo");
+        //se toDo è diverso da null 
+        if (localToDo !== null) {
+            // trasformo la stringa json in un array e lo salvo nel local storage
+            this.toDo = JSON.parse(localToDo);
+        } else {
+            // altrimenti imposto array vuoto nel localstorgare
+            this.toDo =  []; 
+        }
+
     },
     methods: {
        addToDo: function () {
         this.toDo.push({ ...this.newToDo });
         this.newToDo.text = "";
         this.newToDo.done = false;
+
+        // salvare in local storage 
+        //1. trasformo l'array in una stirnga json
+        const jsonToDo = JSON.stringify(this.toDo);
+        //2. salvo la stringa in local storge
+        localStorage.setItem("toDo", jsonToDo);
        }
     }
 }).mount('#app')
